a7262f307177197768e57af00b07f48604e3afbb
WanggoonL <87754634+WanggoonL@users.noreply.github.com>
2022-01-04 16:32:20 +0900

    js.finish
    building css

--- js/todo.js
+++ js/todo.js
@@ -2,22 +2,27 @@ const toDoForm = document.querySelector(".todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+let toDos = [];
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function removeLi(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const btn = document.createElement("button");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   btn.innerText = "X";
   btn.addEventListener("click", removeLi);
   li.appendChild(span);
@@ -29,9 +34,21 @@ function toDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
 toDoForm.addEventListener("submit", toDoSubmit);
+
+const loadToDos = localStorage.getItem(TODOS_KEY);
+
+if (loadToDos !== null) {
+  const parsedToDos = JSON.parse(loadToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "8b09092551c9ffa1cf54d5e4169e4a98";
+
+function onGeoOk(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector(".weather span:first-child");
+      const city = document.querySelector(".weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
+}
+function onGeoErro() {
+  alert("Can't find you :(");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErro);
